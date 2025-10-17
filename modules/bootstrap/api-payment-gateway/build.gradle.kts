8dd1590bcdb974b1e04e886c0b3224818523158c
KangWookOh <kanguk456@gmail.com>
2025-10-17 13:37:50 +0900

    fix: Klint 적용 및 수정

--- modules/bootstrap/api-payment-gateway/build.gradle.kts
+++ modules/bootstrap/api-payment-gateway/build.gradle.kts
@@ -13,20 +13,20 @@ dependencies {
     implementation(projects.modules.external.pgClient)
     implementation(libs.spring.boot.starter.jpa)
     implementation(libs.bundles.bootstrap)
-    
+
     // 선택 과제: 오픈API 문서화 & 운영지표
     implementation(libs.springdoc.openapi.starter.webmvc.ui)
     implementation(libs.spring.boot.starter.actuator)
-    
+
     // 선택 과제: Flyway DB 마이그레이션
     implementation(libs.flyway.core)
     implementation(libs.flyway.mysql)
-    
+
     // H2 데이터베이스: 런타임 (테스트/로컬용)
     runtimeOnly(libs.database.h2)
     // MariaDB: 런타임 (프로덕션용)
     runtimeOnly(libs.database.mariadb)
-    
+
     testImplementation(libs.bundles.test)
     testImplementation(libs.spring.boot.starter.test) {
         exclude(module = "mockito-core")
--- modules/bootstrap/api-payment-gateway/src/main/kotlin/im/bigs/pg/api/payment/PaymentController.kt
+++ modules/bootstrap/api-payment-gateway/src/main/kotlin/im/bigs/pg/api/payment/PaymentController.kt
@@ -11,7 +11,6 @@ import im.bigs.pg.application.payment.port.`in`.QueryPaymentsUseCase
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.media.Content
-import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
